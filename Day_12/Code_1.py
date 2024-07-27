f14798f0d0a42ded2780201c915ad00cb521a040
Dipanshu Maddheshiya <70335021+dm9450-cloud@users.noreply.github.com>
2024-07-27 16:41:04 +0530

    Day_12(Break & Continue)

---
+++ Day_12/Code_1.py
@@ -0,0 +1,14 @@
+# Break & continue
+# Break : Used to terminate the loop when encountered.
+
+# Continue: Terminates execution in the current iteration & Continues execution of the loop with the next iteration.StopIteration
+
+i = 1;
+while i <=5:
+    print(i);
+    if(i == 3):
+        break;
+    
+    i += 1;
+
+print("End of loop ");
